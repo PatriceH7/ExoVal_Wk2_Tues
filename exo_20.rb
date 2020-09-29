a9b0776cbba4e57577c273f3709cbee09eaa2ea4
PatriceH7 <patricehuszezo@gmail.com>
2020-09-30 01:19:04 +0200

    2.20 La pyramide - v2 enlève ligne espace superflu

--- exo_20.rb
+++ exo_20.rb
@@ -10,13 +10,13 @@ number_levels = gets.chomp
 
 print "Voici la pyramide :"
 
-(number_levels.to_i + 1).times do |i| puts 
-	i.times do print "#"	
+(number_levels.to_i).times do |i| puts
+	number_display = i + 1
+	(number_display.to_i).times do print "#"	
 	end
 end
 
 puts
-
 
 
 
