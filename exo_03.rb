91704070802ae9bbb1a5ae097a254b3f19286f2a
PatriceH7 <patricehuszezo@gmail.com>
2020-09-29 18:17:42 +0200

    2.3 Il ne dit pas bonjour

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+# exo_03.rb
+# puts "Bonjour, monde !"
+puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# le résultat = la 1ère ligne n'est plus affichée, car elle est passée en commentaire du fait du #
