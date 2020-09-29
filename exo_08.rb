2e1fcaf969055a98b0999bae1f54884403208d59
PatriceH7 <patricehuszezo@gmail.com>
2020-09-29 20:33:20 +0200

    2.8 Un programme qui dit bonjour

---
+++ exo_08.rb
@@ -0,0 +1,10 @@
+# exo_08.rb
+
+# demande le prénom de l'utilisateur, 
+# et salue l'utilisateur avec "Bonjour, prénom !"
+
+puts "Quel est votre prénom ?"
+print "> "
+user_first_name = gets.chomp
+puts "Bonjour, #{user_first_name} !"
+
