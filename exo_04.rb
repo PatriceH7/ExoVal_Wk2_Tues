93ec170f6171eabda756097c3059b242264643fd
PatriceH7 <patricehuszezo@gmail.com>
2020-09-29 18:28:46 +0200

    2.4 Ca marche pô

--- exo_04.rb
+++ exo_04.rb
@@ -2,7 +2,8 @@
 
 # exo_04.rb:2: on obtient le message d'erreur 
 # "unterminated string meets end of file"
-# du fait de l'absence de fermeture des guillemets après le point d'interrogation
+# du fait de l'absence de fermeture des guillemets 
+# après le point d'interrogation
 
 puts "Salut, ça farte ?
 
