e8caa1e547efa0eadeaa114e16b6b1a4c6d479a7
PatriceH7 <patricehuszezo@gmail.com>
2020-09-29 23:44:08 +0200

    2.18 Une liste d'email

---
+++ exo_18.rb
@@ -0,0 +1,10 @@
+# exo_18.rb
+
+# Écris un programme exo_18.rb qui va 
+# créer une liste de 50 faux emails et les stocker dans une array. 
+
+liste_emails = Array.new(50)
+
+50.times do |i|
+	liste_emails[i] == "jean.dupont.#{i}@email.fr"
+end
