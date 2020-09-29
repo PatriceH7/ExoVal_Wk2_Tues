f14921dd6df345a0fa1e1a9c89df112d9d1f2ecd
PatriceH7 <patricehuszezo@gmail.com>
2020-09-29 22:37:26 +0200

    2.16 Virer les données - en enlevant les années

--- exo_16.rb
+++ exo_16.rb
@@ -9,10 +9,7 @@ print "> "
 user_birth_year = gets.chomp
 
 (2020 - user_birth_year.to_i).times do |i|
-	print "En "
-	print user_birth_year.to_i + 1 + i
-	print ", "
-	print "il y a "
+	print "Il y a "
 	print 2020 - user_birth_year.to_i - i - 1
 	print " ans, tu avais "
 	print 1 + i
