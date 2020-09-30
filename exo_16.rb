e2790a8e00051c3a063b1ecf97408776e3842ec7
PatriceH7 <patricehuszezo@gmail.com>
2020-09-30 02:03:40 +0200

    2.16 Virer les années - v3 modifié pour démarrer de l'âge

--- exo_16.rb
+++ exo_16.rb
@@ -4,16 +4,29 @@
 # et pour chaque année depuis sa naissance, dire
 # "Il y a X ans, tu avais Y ans !
 
-puts "Quelle est ton année de naissance ?"
+puts "Quel est ton âge ?"
 print "> "
-user_birth_year = gets.chomp
+age = gets.chomp
+# year_of_birth = 2020 - age.to_i
 
-(2020 - user_birth_year.to_i).times do |i|
+# version corrigée pour partir directement de l'âge
+
+(age.to_i).times do |i|
 	print "Il y a "
-	print 2020 - user_birth_year.to_i - i - 1
-	print " ans, tu avais "
-	print 1 + i
+	print age.to_i - i
+	print " ans, to avais "
+	print i 
 	puts " ans !"
+		
 end
+
+
+#(2020 - user_birth_year.to_i).times do |i|
+#	print "Il y a "
+#	print 2020 - user_birth_year.to_i - i - 1
+#	print " ans, tu avais "
+#	print 1 + i
+#	puts " ans !"
+#end
 
 # 2nde version ds laquelle on a effectivement enlevé les années
